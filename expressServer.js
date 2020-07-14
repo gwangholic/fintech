cbe4597643dfec1640cec5ef78bd5c8d3caa254b
gwangholic <gafordev@gmail.com>
2020-07-14 11:57:04 +0900

    오픈뱅킹 api 사용법 회원가입 페이지

--- expressServer.js
+++ expressServer.js
@@ -14,26 +14,8 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 })
  
-app.get("/ejsTest", function (req, res) {
-  res.render("test");
-})
+app.get('/signup', function(req, res) {
+  res.render("signup");
+});
 
-app.get("/designTest", function (req, res) {
-  res.render("designSample.ejs")
-})
-
-app.post("/ajaxTest", function (req, res) {
-  var userId = req.body.sendUserId;
-  var userPassword = req.body.sendUserPassword;
-  console.log("요청 바디 :", req.body);
-  console.log("사용자 아이디는 :", userId);
-  console.log("사용자 password :", userPassword);
-
-  res.json("로그인에 성공하셨습니다.")
-})
-
-app.get("/appRouter", function (req, res){
-  console.log("router working");
-  res.send("<html><h1>안녕하세요 새로 추가된 라우터 입니다.</h1></html>");
-})
 app.listen(5500);
